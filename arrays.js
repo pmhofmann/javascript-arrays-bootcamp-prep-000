8c7a79ec242105a501efb2b9c10bdc45e93d4fa1
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2018-10-21 08:40:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return(array);
 }
 function removeElementFromBeginningOfArray(array){
-  array.slice(1);
+  return(array.slice(1));
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
