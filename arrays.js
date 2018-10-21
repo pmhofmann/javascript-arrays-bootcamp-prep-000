be4488384448eef7de93da4a63514f75c0e40bfc
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2018-10-21 08:42:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return(array);
 }
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.length -1);
-  return
+  return(array.slice(0, array.length -1));
+  
 }
